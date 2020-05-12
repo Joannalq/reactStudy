82f50c5c9431e50f965019e34c801b46f1962ad3
Joannalq <joanna851026@hotmail.com>
2020-05-12 21:51:18 +1000

    using React Redux Form

---
+++ src/redux/reducer.js
@@ -0,0 +1,15 @@
+import { DISHES } from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { LEADERS } from '../shared/leaders';
+import { PROMOTIONS } from '../shared/promotions';
+
+export const initialstate = {
+    dishes: DISHES,
+    comments: COMMENTS,
+    leaders: LEADERS,
+    promotions: PROMOTIONS,
+};
+
+export const Reducer = (state = initialstate,action) => {
+    return state;
+};
