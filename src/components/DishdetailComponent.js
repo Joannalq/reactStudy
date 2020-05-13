c41bad6e56dba4f9dcc88cdeae75f35633b00b1c
Joannalq <joanna851026@hotmail.com>
2020-05-13 22:25:35 +1000

    assignment 3

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,8 +1,92 @@
-import React from 'react'
-import {Card, CardImg, CardText,CardBody,CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap'
+import React,{Component} from 'react'
+import {Card, CardImg, CardText,CardBody,CardTitle, Breadcrumb, BreadcrumbItem,Button,Row, Label,Modal, ModalHeader, ModalBody, Col} from 'reactstrap'
 import {Link} from 'react-router-dom'
+import {Control, LocalForm, Errors} from 'react-redux-form'
+const required = (val) => val && val.length;
+const maxLength = (len) => (val)=>!(val)||(val.length<=len);
+const minLength = (len) => (val)=>val&&(val.length>=len);
+class CommentForm extends Component{
+    constructor(props){
+        super(props);
+        this.state={
+            isModalOpen:false
+        };
+        this.toggleModal = this.toggleModal.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-    
+    toggleModal(){
+        this.setState({
+            isModalOpen:!this.state.isModalOpen
+        });
+    }
+    handleSubmit(event){
+        this.toggleModal();
+        alert("Current state is: "+JSON.stringify(event));
+        // event.preventDefault();
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                <Button className="bg-white text-dark" onClick={this.toggleModal}><i className="fa fa-pencil fa-lg"></i>{' '}Submit Comment</Button>
+                <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+                    <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+                    <ModalBody>
+                        <LocalForm onSubmit={(values)=> this.handleSubmit(values)}>
+                            <Row className="form-group">
+                                <Label htmlFor="rating"  md={12}>Rating</Label>
+                                <Col>
+                                    <Control.select model=".rating" name="rating" className="form-control">
+                                        <option>1</option>
+                                        <option>2</option>
+                                        <option>3</option>
+                                        <option>4</option>
+                                        <option>5</option>
+                                        <option>6</option>
+                                    </Control.select>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="author" md={12}>Your Name</Label>
+                                <Col>
+                                    <Control.text model=".author" name="author" id="author" placeholder="your name" 
+                                    className="form-control"
+                                    validators = {
+                                        {
+                                            required, minLength: minLength(3), maxLength:maxLength(15)
+                                        }
+                                    }
+                                    />
+                                    <Errors className="text-danger" model=".author" show="touched" 
+                                    messages={{
+                                        required:'Required',
+                                        minLength: 'Must be greater than 2 characters',
+                                        maxLength:'Must be 15 characters or less'
+                                    }}></Errors>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Label htmlFor="comment"  md={12}>Comment</Label>
+                                <Col>
+                                    <Control.textarea model=".comment" name="comment" id="comment" rows="6" className="form-control"/>
+                                </Col>
+                            </Row>
+                            <Row className="form-group">
+                                <Col md={{size:10, offset:2}}>
+                                    <Button type="submit" color="primary">Submit</Button>
+                                </Col>
+                            </Row>
+                        </LocalForm>
+                    </ModalBody>
+                </Modal>
+            </React.Fragment>
+            
+        );
+    }
+}
+
+
     function RenderDish({dish}){
         return(
             <Card>
@@ -21,7 +105,7 @@ import {Link} from 'react-router-dom'
             return (  
                 <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
-                    <ul className="list-unstyled">
+                    <ul className='list-unstyled'>
                         {comments.map((comment)=>{
                             return(
                                 <li key={comment.id}>
@@ -31,6 +115,7 @@ import {Link} from 'react-router-dom'
                             );
                         })}
                     </ul>
+                    <CommentForm/>
                 </div>      
             );
         }else{
@@ -38,6 +123,8 @@ import {Link} from 'react-router-dom'
                 <div></div>
             )
         }
+
+
     } 
 
     const DishDetail = (props) => {
@@ -60,18 +147,18 @@ import {Link} from 'react-router-dom'
                         <div className="col-12 col-md-5 m-1">
                             <RenderDish dish={props.dish}/>
                         </div>
-                        <RenderComments comments={props.comments}/>    
+                        <RenderComments comments={props.comments} /> 
+                        
                     </div>
                 </div>
             );
-        }else{
+        }
+        else{
             return(
                 <div></div>
             );
         }  
     }
-               
     
-
-
 export default DishDetail;
+
