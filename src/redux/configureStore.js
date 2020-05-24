6bb0401c9d86beec84b1694ef16897b6dbfc14a8
Joannalq <joanna851026@hotmail.com>
2020-05-24 18:14:10 +1000

    react redux form

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import {createStore, combineReducers, applyMiddleware} from 'redux';
+import{createForms} from 'react-redux-form';
 import {Dishes} from './dishes';
 import {Comments} from './comments';
 import {Leaders} from './leaders';
 import {Promotions} from './promotions';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
+import { InitialFeedback } from './forms';
 
 export const ConfigureStore = () => {
     // recomposes the global state for the application
@@ -13,7 +15,11 @@ export const ConfigureStore = () => {
             dishes:Dishes,
             comments:Comments,
             promotions: Promotions,
-            leaders: Leaders
+            leaders: Leaders,
+            //add reducer function and the state information into create store
+            ...createForms({
+                feedback:InitialFeedback
+            })
         }),
         //both thunk and logger will be available for our application
         applyMiddleware(thunk, logger)
